bca2cb21ead045cb317bde9cc125ed088f65bc5c
Jeff Hykin <jeff.hykin@gmail.com>
2024-09-24 16:45:13 -0500

    -

--- tests/cli_1.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-deno run -A ../main/to-esm.js -- ../test/input_file.js
-mv ../test/input_file.esm.js ../logs/input_file.esm.js
-
-deno run -A ../main/to-esm.js --recursive --add-ext .other -- ../test/
-mv ../test/input_file.other.js ../logs/input_file.other.js
-rm -f ../test/input_file.other.ts
